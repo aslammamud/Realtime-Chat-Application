bcb5dcd8273e5b898be50befa997f1ea8bf61d23
Aslam Mamud <aslammamud@gmail.com>
2020-11-09 13:27:18 +0600

    React Hook and Emit

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 
-import Join from './components/Join';
-import Chat from './components/Chat';
+import Join from './components/Join/Join';
+import Chat from './components/Chat/Chat';
 
 
 const App = () => (
--- server/index.js
+++ server/index.js
@@ -9,5 +9,19 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+io.on('connecton',(socket) => {
+  console.log('We have a new connection!');
+
+  socket.on('join',({ name, room }, callback) =>{
+    console.log(name, room);
+
+    callback();
+  });
+
+  socket.on('disconnect',() => {
+    console.log('User left');
+  });
+});
+
 app.use(router);
 server.listen(PORT, ()=> console.log(`Server has started on port ${PORT}`));

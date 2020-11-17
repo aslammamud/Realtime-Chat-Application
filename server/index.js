5d0282e37eab4fba9926c53f3cbe153adb2d8b9f
aslammamud <aslammamud@gmail.com>
2020-11-18 01:41:42 +0600

    Changes in Server - index.js

--- server/index.js
+++ server/index.js
@@ -1,25 +1,31 @@
+const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
-const http = require('http');
+const cors = require('cors');
 
-const { adduser,removeUser,getUser,getUserInRoom } = require('./users.js');
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 
-const PORT = process.env.PORT || 5000;
 const router = require('./router');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-io.on('connecton',(socket) => {
-  socket.on('join',({ name, room }, callback) =>{
-    const { error,user } = adduser({ id: socket.id,name,room });
+app.use(cors());
+app.use(router);
+
+io.on('connect', (socket) => {
+  socket.on('join', ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
 
     if(error) return callback(error);
 
-    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the room ${user.room}`});
-    socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name}, has joined `});
     socket.join(user.room);
+
+    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.`});
+    socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
+
+    io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
 
     callback();
   });
@@ -27,15 +33,19 @@ io.on('connecton',(socket) => {
   socket.on('sendMessage', (message, callback) => {
     const user = getUser(socket.id);
 
-    io.to(user.room).emit('message', { user: user.name , text: message});
+    io.to(user.room).emit('message', { user: user.name, text: message });
 
     callback();
   });
 
-  socket.on('disconnect',() => {
-    console.log('User left');
-  });
+  socket.on('disconnect', () => {
+    const user = removeUser(socket.id);
+
+    if(user) {
+      io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
+      io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+    }
+  })
 });
 
-app.use(router);
-server.listen(PORT, ()=> console.log(`Server has started on port ${PORT}`));
+server.listen(process.env.PORT || 5000, () => console.log(`Server has started.`));
--- server/users.js
+++ server/users.js
@@ -1,31 +1,29 @@
 const users = [];
 
-const adduser = ({id,name,room}) => {
+const addUser = ({ id, name, room }) => {
   name = name.trim().toLowerCase();
   room = room.trim().toLowerCase();
 
-  const existingUser = users.find((user)=>user.room === room && user.name === name);
+  const existingUser = users.find((user) => user.room === room && user.name === name);
 
-  if(existingUser) {
-    return { error: 'Username is taken' }
-  }
+  if(!name || !room) return { error: 'Username and room are required.' };
+  if(existingUser) return { error: 'Username is taken.' };
 
-  const user = { id,name,room };
+  const user = { id, name, room };
+
   users.push(user);
 
-  return { user }
+  return { user };
 }
 
-const removeUser = () => {
-  const index = users.findIndex((user)=> user.id === id);
+const removeUser = (id) => {
+  const index = users.findIndex((user) => user.id === id);
 
-  if(index !== -1){
-    return users.splice(index, 1)[0];
-  }
+  if(index !== -1) return users.splice(index, 1)[0];
 }
 
-const getUser = ()=> users.find((user) => user.id === id);
+const getUser = (id) => users.find((user) => user.id === id);
 
-const getUserInRoom = (room) => users.filter((user) => user.room === room);
+const getUsersInRoom = (room) => users.filter((user) => user.room === room);
 
-module.exports = { adduser,removeUser,getUser,getUserInRoom };
+module.exports = { addUser, removeUser, getUser, getUsersInRoom }

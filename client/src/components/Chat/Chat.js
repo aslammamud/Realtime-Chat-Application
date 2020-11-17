2f11871c5127d2fc555b313381ff570fc43c5555
aslammamud <aslammamud@gmail.com>
2020-11-18 01:40:22 +0600

    Updated Chat.js

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,66 +1,66 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import queryString from 'query-string';
-import io from 'socket.io-client'
+import io from "socket.io-client";
 
+import Messages from '../Messages/Messages';
 import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input';
 
 import './Chat.css';
 
+const ENDPOINT = 'localhost:5000';
+
 let socket;
 
-const Chat  = ({ location })=> {
+const Chat = ({ location }) => {
   const [name, setName] = useState('');
   const [room, setRoom] = useState('');
+  const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
 
-
-  const ENDPOINT = 'localhost:5000';
-
   useEffect(() => {
-    const { name,room } = queryString.parse(location.search);
+    const { name, room } = queryString.parse(location.search);
 
     socket = io(ENDPOINT);
-    setName(name);
+
     setRoom(room);
+    setName(name)
 
-    socket.emit('join',{ name, room },()=>{
+    socket.emit('join', { name, room }, (error) => {
+      if(error) {
+        alert(error);
+      }
+    });
+  }, [location.search]);
 
+  useEffect(() => {
+    socket.on('message', message => {
+      setMessages(messages => [ ...messages, message ]);
     });
 
-    return ()=> {
-      socket.emit('diconnect');
-
-      socket.off();
-    }
-  },[ENDPOINT,location.search]);
-
-  useEffect(()=>{
-    socket.on('message', (message)=>{
-      setMessages([...messages,message]);
+    socket.on("roomData", ({ users }) => {
+      setUsers(users);
     });
-  },[messages]);
+}, []);
 
   const sendMessage = (event) => {
     event.preventDefault();
+
     if(message) {
-      socket.emit('sendMessage', message, ()=> setMessage(''));
+      socket.emit('sendMessage', message, () => setMessage(''));
     }
   }
 
-  console.log(message, messages);
   return (
-  <div className="outerContainer">
-    <div className="container">
-      <InfoBar room={room} />
-      <input
-        value={message}
-        onChange={(event)=> setMessage(event.target.value)}
-        onKeyPress={event => event.key ===  'Enter' ? sendMessage(event) : null}
-        />
+    <div className="outerContainer">
+      <div className="container">
+          <InfoBar room={room} />
+          <Messages messages={messages} name={name} />
+          <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
+      </div>
     </div>
-  </div>
-  )
+  );
 }
 
 export default Chat;
